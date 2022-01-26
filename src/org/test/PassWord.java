f8292a42d5d380ca74a444c87f4b0c3ec67ee591
Dineshvelu94 <dienshvelu22@gmail.com>
2022-01-26 15:57:17 -0800

    Master Task

---
+++ src/org/test/PassWord.java
@@ -0,0 +1,9 @@
+package org.test;
+
+public class PassWord {
+private void password() {
+System.out.println("PassWord ");
+}
+
+
+}
---
+++ src/org/testone/UserName.java
@@ -0,0 +1,7 @@
+package org.testone;
+
+public class UserName {
+private void login() {
+System.out.println("Log In Method");
+}
+}
