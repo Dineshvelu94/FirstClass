2e05f7be105ed10b819469040677a668ca307e24
Dineshvelu94 <dienshvelu22@gmail.com>
2022-01-26 16:24:49 -0800

    yashwanth task

--- src/org/test/PassWord.java
+++ src/org/test/PassWord.java
@@ -3,6 +3,10 @@ package org.test;
 public class PassWord {
 private void password() {
 System.out.println("PassWord ");
+int a =20;
+int b = 30;
+System.out.println(a+b);
+System.out.println("PassWord ");
 }
 
 
